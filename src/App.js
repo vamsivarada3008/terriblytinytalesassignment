0471b986647f0ee860a63666074f4c31c62384b5
vamsi <vamsivarada3008@gmail.com>
2023-05-14 12:08:20 +0530

    final updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
+import { BarChart, Bar,  XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 function App() {
+  const [wordCounts, setWordCounts] = useState(null);
+
+  const fetchWordCounts = async () => {
+    const response = await fetch("https://www.terriblytinytales.com/test.txt");
+    const text = await response.text();
+    const words = text.trim().toLowerCase().split(/\s+/);
+    const counts = {};
+    words.forEach((word) => {
+      counts[word] = (counts[word] || 0) + 1;
+    });
+
+    const entries = Object.entries(counts);
+    const sortedEntries = entries.sort(([, a], [, b]) => b - a);
+    const slicedEntries = sortedEntries.slice(0, 20);
+    const sortedCounts = slicedEntries.map(([word, count]) => ({ word, count }));
+    setWordCounts(sortedCounts);
+    
+
+
+  };
+
+  const exportCSV = () => {
+    const csv = "Word,Count\n" +
+      wordCounts.map(({ word, count }) => `${word},${count}`).join("\n");
+    const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", "histogram.csv");
+    link.style.display = "none";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {wordCounts ? (
+        <>
+          <h2>Histogram</h2>
+          
+          <ResponsiveContainer width="80%" height={400}>
+            <BarChart
+              width={900}
+              height={400}
+              data={wordCounts}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="word" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="count" fill="#8884d8" />
+             
+            </BarChart>
+            
+         </ResponsiveContainer>
+         <button className="export" onClick={exportCSV}>Export CSV</button>
+          
+        </>
+      ) : (
+        <button
+          className="gradientButton"
+          onClick={fetchWordCounts}>Submit</button>
+      )}
     </div>
   );
 }
